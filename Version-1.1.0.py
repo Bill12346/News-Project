4954c92504560859ee256a32a9116facbe208450
Bill12346 <2288598@gmail.com>
2023-01-29 13:15:23 -0500

    Version 1.2.0
    Fixed when the terminal closes right after completion (instant) making it inposible to read

---
+++ Version-1.1.0.py
@@ -0,0 +1,25 @@
+import time
+
+import requests
+
+
+api_key = "fccfcb87844246cb81c99b8ed6030de5"
+
+
+url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + api_key
+response = requests.get(url)
+
+
+data = response.json()
+for i, article in enumerate(data["articles"]):
+    if i >= 5:
+        break
+    print(f"{i + 1}. {article['title']}")
+    time.sleep(0.2)
+
+
+
+
+
+
+
